0243d79d25c3f89cdbbf5f8e70bf188642021a5f
Felix Dulfer <felix@dulfer.me>
2025-07-10 17:40:29 +0200

    Remove ngx-mat-quill application and related configurations; add Angular component style configuration in nx.json.

---
+++ apps/demo-app/src/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quill-editor',
+  imports: [CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class QuillEditor {}
