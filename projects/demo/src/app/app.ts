463d7aca8d02dd5a1c4b60cb5cebe97d37d05217
Felix Dulfer <felix@dulfer.me>
2025-07-12 17:25:33 +0200

    feat: integrate Azure Blue theme and Material icons; enhance app layout with Material form fields and typography

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NgxMatQuill } from 'ngx-mat-quill';
+import { NgxMatQuill, MatQuillFormFieldControlDirective } from 'ngx-mat-quill';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
@@ -10,6 +10,7 @@ import { JsonPipe } from '@angular/common';
     RouterModule,
     MatFormFieldModule,
     NgxMatQuill,
+    MatQuillFormFieldControlDirective,
     ReactiveFormsModule,
     JsonPipe,
   ],
